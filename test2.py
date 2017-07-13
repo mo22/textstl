e663cfae637792a1286aa23df5c73310f8c295eb
Moritz Moeller <mm@mxs.de>
2017-07-13 19:39:50 +0200

    ..

--- test2.py
+++ test2.py
@@ -7,6 +7,13 @@ def vec_sub(a, b):
   assert len(a) == len(b)
   return tuple([ a[i] - b[i] for i in range(len(a)) ])
 
+def vec_add(a, b):
+  assert len(a) == len(b)
+  return tuple([ a[i] + b[i] for i in range(len(a)) ])
+
+def vec_scale(a, s):
+  return tuple([ a[i] * s for i in range(len(a)) ])
+
 def vec_cross(u, v):
   assert len(u) == len(v)
   dim = len(u)
@@ -23,6 +30,8 @@ def vec_cross(u, v):
       s.append(u[j]*v[k] - u[k]*v[j])
   return s
 
+def vec_len(a):
+  return sum([ i**2 for i in a ]) ** 0.5
 
 
 triangles = []
@@ -31,7 +40,7 @@ triangles += [
   ( (1.0, 1.0, 1.0), (0.0, 1.0, 1.0), (0.0, 0.0, 1.0) ),
 ]
 triangles = [
-  ((ax*100, ay*100, az*100), (bx*100, by*100, bz*100), (cx*100, cy*100, cz*100)) for ((ax, ay, az), (bx, by, bz), (cx, cy, cz)) in triangles[0:2]
+  ((ax*10, ay*10, az*10), (bx*10, by*10, bz*10), (cx*10, cy*10, cz*10)) for ((ax, ay, az), (bx, by, bz), (cx, cy, cz)) in triangles[0:2]
 ]
 # mirror z axis
 triangles += [
@@ -50,14 +59,15 @@ with open('test.stl', 'wb') as fp:
   fp.write(header)
   fp.write(struct.pack('<I', len(triangles)))
   for triangle in triangles:
-    n1 = vec_sub(triangle[0], triangle[1])
-    n2 = vec_sub(triangle[1], triangle[2])
+    n1 = vec_sub(tri[0], tri[1])
+    n2 = vec_sub(tri[1], tri[2])
     n = vec_cross(n1, n2)
+    n = vec_scale(n, 1 / vec_len(n))
     fp.write(struct.pack('<fff', n[0], n[1], n[2]))
-    fp.write(struct.pack('<fff', triangle[0][0], triangle[0][1], triangle[0][2]))
-    fp.write(struct.pack('<fff', triangle[1][0], triangle[1][1], triangle[1][2]))
-    fp.write(struct.pack('<fff', triangle[2][0], triangle[2][1], triangle[2][2]))
-    fp.write(struct.pack('<H', 20083))
+    fp.write(struct.pack('<fff', tri[0][0], tri[0][1], tri[0][2]))
+    fp.write(struct.pack('<fff', tri[1][0], tri[1][1], tri[1][2]))
+    fp.write(struct.pack('<fff', tri[2][0], tri[2][1], tri[2][2]))
+    fp.write(struct.pack('<H', 0)) # 20083
 
 if False:
   with open('noah1.stl', 'rb') as fp:
