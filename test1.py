bf951ab15ed84b876a5344ba226ece295cbc8506
Moritz Moeller <mm@mxs.de>
2017-07-17 12:42:31 +0200

    ..

--- test1.py
+++ test1.py
@@ -1,9 +1,10 @@
 from __future__ import print_function
 import freetype
 import math
-import scipy.spatial
-import numpy as np
-import matplotlib.pyplot as plt
+# import scipy.spatial
+import numpy
+import stl
+# import matplotlib.pyplot as plt
 
 
 
@@ -87,74 +88,185 @@ if False:
 
 
 face.set_char_size(100)
-face.load_char('l')
-# face.load_char('8')
+# face.load_char('0')
+face.load_char('8')
 # face.load_char('i')
 
 
-
-
-
-
-polygons = []
-start = 0
-end = 0
-for i in range(len(face.glyph.outline.contours)):
-    end = face.glyph.outline.contours[i]
-    points = face.glyph.outline.points[start:end+1]
-    points.append(points[0])
-    print("XXX", i, is_clockwise(points))
-    polygons.append(points)
-    # tags: 1 = PATH_POINT, 2 = CUBIC_POINT, 0 = QUADRATIC_POINT
-    """
-    tags = face.glyph.outline.tags[start:end+1]
-    tags.append(tags[0])
-    segments = [ [points[0],], ]
-    for j in range(1, len(points) ):
-        segments[-1].append(points[j])
-        if tags[j] & (1 << 0) and j < (len(points)-1):
-            segments.append( [points[j],] )
-    verts = [points[0], ]
-    for segment in segments:
-        if len(segment) == 2:
-            verts.extend(segment[1:])
-            #codes.extend(['LINETO'])
-        elif len(segment) == 3:
-            verts.extend(segment[1:])
-            #codes.extend(['CURVE3', 'CURVE3'])
+# return list of segments and holes, each list of (x, y), not closed.
+def process_glyph(face):
+    segments = []
+    holes = []
+    start = 0
+    for i in range(len(face.glyph.outline.contours)):
+        end = face.glyph.outline.contours[i]
+        polypoints = face.glyph.outline.points[start:end+1]
+        polypoints.append(polypoints[0])
+        if is_clockwise(polypoints):
+            segments.append(face.glyph.outline.points[start:end+1])
         else:
-            verts.append(segment[1])
-            #codes.append('CURVE3')
-            for i in range(1,len(segment)-2):
-                A,B = segment[i], segment[i+1]
-                C = ((A[0]+B[0])/2.0, (A[1]+B[1])/2.0)
-                verts.extend([ C, B ])
-                #codes.extend([ 'CURVE3', 'CURVE3'])
-            verts.append(segment[-1])
-            #codes.append('CURVE3')
-    polygons.append(verts)
-    """
-    start = end+1
+            holes.append(face.glyph.outline.points[start:end+1])
+        start = end
+    return (segments, holes)
+
+# generate list of triangle coordinates ((x, y, z), (x, y, z), (x, y, z))
+def process_earcut(segment, holes):
+    points = []
+    hole_indices = []
+    for (x, y) in segment:
+        points.append(x)
+        points.append(y)
+    for hole in holes:
+        hole_indices.append(len(points)/2)
+        for (x, y) in hole:
+            points.append(x)
+            points.append(y)
+    print("earcut", len(points), hole_indices)
+    import earcut
+    tris = earcut.earcut(points, hole_indices, 2)
+    res = []
+    for i in range(0, len(tris), 3):
+        (a, b, c) = tris[i:i+3]
+        res.append((
+            (points[a*2+0], points[a*2+1]),
+            (points[b*2+0], points[b*2+1]),
+            (points[c*2+0], points[c*2+1]),
+        ))
+    return res
+
+
+def process_sides(segments, holes, width):
+    res = []
+    for segment in segments + holes:
+        j = len(segment)-1
+        for i in range(len(segment)):
+            # create two triangles
+            (ix, iy) = segment[i]
+            (lx, ly) = segment[j]
+            res.append((
+                (lx, ly, +width/2),
+                (ix, iy, +width/2),
+                (lx, ly, -width/2)
+            ))
+            res.append((
+                (lx, ly, -width/2),
+                (ix, iy, +width/2),
+                (ix, iy, -width/2)
+            ))
+            j = i
+    return res
+
+
+width = 5
+
+(segments, holes) = process_glyph(face)
+
+triangles = []
+for segment in segments:
+    triangles += process_earcut(segment, holes)
+
+top = []
+bottom = []
+for (ax, ay), (bx, by), (cx, cy) in triangles:
+    bottom.append((
+        (ax, ay, -width/2),
+        (bx, by, -width/2),
+        (cx, cy, -width/2)
+    ))
+    top.append((
+        (ax, ay, width/2),
+        (bx, by, width/2),
+        (cx, cy, width/2)
+    ))
+
+sides = process_sides(segments, holes, width)
+
+
+data = numpy.zeros(len(top)+len(bottom)+len(sides), dtype=stl.mesh.Mesh.dtype)
+data['vectors'] = numpy.array(top + bottom + sides)
+# data = numpy.zeros(len(triangles)*2+len(sides), dtype=stl.mesh.Mesh.dtype)
+# for i, ((ax, ay), (bx, by), (cx, cy)) in enumerate(triangles):
+#   data['vectors'][i*2+0] = numpy.array([
+#     [ax, ay, -width/2],
+#     [bx, by, -width/2],
+#     [cx, cy, -width/2]
+#   ])
+#   data['vectors'][i*2+1] = numpy.array([
+#     [ax, ay, width/2],
+#     [bx, by, width/2],
+#     [cx, cy, width/2]
+#   ])
+mesh = stl.mesh.Mesh(data)
+mesh.save('test.stl')
 
 
 
-# import polytri
-import triangulate
-import polygon
 
-for verts in polygons:
-    p = polygon.SimplePolygon(verts[0:-1], True)
-    x = triangulate.Triangulate([], p)
-    print( x.triangulate() )
-    # x = list( polytri.triangulate(verts[0:-1]) )
-    # print( x )
 
-    verts = np.array(verts)
 
-    tri = scipy.spatial.Delaunay(verts)
-    plt.triplot(verts[:,0], verts[:,1], tri.simplices.copy())
 
-    plt.plot(verts[:,0], verts[:,1])
 
-plt.show()
+if False:
+
+    polygons = []
+    start = 0
+    end = 0
+    for i in range(len(face.glyph.outline.contours)):
+        end = face.glyph.outline.contours[i]
+        points = face.glyph.outline.points[start:end+1]
+        points.append(points[0])
+        print("XXX", i, is_clockwise(points))
+        polygons.append(points)
+        # tags: 1 = PATH_POINT, 2 = CUBIC_POINT, 0 = QUADRATIC_POINT
+        """
+        tags = face.glyph.outline.tags[start:end+1]
+        tags.append(tags[0])
+        segments = [ [points[0],], ]
+        for j in range(1, len(points) ):
+            segments[-1].append(points[j])
+            if tags[j] & (1 << 0) and j < (len(points)-1):
+                segments.append( [points[j],] )
+        verts = [points[0], ]
+        for segment in segments:
+            if len(segment) == 2:
+                verts.extend(segment[1:])
+                #codes.extend(['LINETO'])
+            elif len(segment) == 3:
+                verts.extend(segment[1:])
+                #codes.extend(['CURVE3', 'CURVE3'])
+            else:
+                verts.append(segment[1])
+                #codes.append('CURVE3')
+                for i in range(1,len(segment)-2):
+                    A,B = segment[i], segment[i+1]
+                    C = ((A[0]+B[0])/2.0, (A[1]+B[1])/2.0)
+                    verts.extend([ C, B ])
+                    #codes.extend([ 'CURVE3', 'CURVE3'])
+                verts.append(segment[-1])
+                #codes.append('CURVE3')
+        polygons.append(verts)
+        """
+        start = end+1
+
+
+
+    # import polytri
+    import triangulate
+    import polygon
+
+    for verts in polygons:
+        p = polygon.SimplePolygon(verts[0:-1], True)
+        x = triangulate.Triangulate([], p)
+        print( x.triangulate() )
+        # x = list( polytri.triangulate(verts[0:-1]) )
+        # print( x )
+
+        verts = np.array(verts)
+
+        tri = scipy.spatial.Delaunay(verts)
+        plt.triplot(verts[:,0], verts[:,1], tri.simplices.copy())
+
+        plt.plot(verts[:,0], verts[:,1])
+
+    plt.show()
 
--- test6.py
+++ test6.py
@@ -1,32 +1,59 @@
 import stl
 import math
 import numpy
-import matplotlib.tri
-import scipy.spatial
+# import matplotlib.tri
+# import scipy.spatial
 
 # polygon
 # create shape
 
-polygon = numpy.array([
+# polygon = numpy.array([
+#   [0, 0],
+#   [4, 0],
+#   [4, 1],
+#   [3, 2],
+#   [3, 1],
+#   [0, 1]
+# ])
+
+polygon = [
   [0, 0],
   [4, 0],
   [4, 1],
   [3, 2],
   [3, 1],
   [0, 1]
-])
+]
 
+polygon_flat = [item for sublist in polygon for item in sublist]
+print(polygon_flat)
 
-tess = scipy.spatial.Delaunay(polygon)
+import earcut
+x = earcut.earcut(polygon_flat, None, 2)
+print(x)
 
-data = numpy.zeros(len(tess.vertices), dtype=stl.mesh.Mesh.dtype)
-for i, (a, b, c) in enumerate(tess.vertices):
-  print i, a, b, c
-  data['vectors'][i] = numpy.array([
-    [tess.points[a][0], tess.points[a][1], 0],
-    [tess.points[b][0], tess.points[b][1], 0],
-    [tess.points[c][0], tess.points[c][1], 0]
+data = numpy.zeros(len(x)/3, dtype=stl.mesh.Mesh.dtype)
+for i in range(0, len(x), 3):
+  (a, b, c) = x[i:i+3]
+  print(a, b, c)
+  data['vectors'][i/3] = numpy.array([
+    [polygon[a][0], polygon[a][1], 0],
+    [polygon[b][0], polygon[b][1], 0],
+    [polygon[c][0], polygon[c][1], 0]
   ])
+
+
+
+# tess = scipy.spatial.Delaunay(polygon)
+
+# data = numpy.zeros(len(tess.vertices), dtype=stl.mesh.Mesh.dtype)
+# for i, (a, b, c) in enumerate(tess.vertices):
+#   print i, a, b, c
+#   data['vectors'][i] = numpy.array([
+#     [tess.points[a][0], tess.points[a][1], 0],
+#     [tess.points[b][0], tess.points[b][1], 0],
+#     [tess.points[c][0], tess.points[c][1], 0]
+#   ])
 
 
 # triang = matplotlib.tri.Triangulation(polygon[:,0], polygon[:,1])
