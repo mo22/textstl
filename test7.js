1656d0d42ebad4443289332496ab4f6a1e987400
Moritz Moeller <mm@mxs.de>
2017-07-18 12:52:43 +0200

    ..

--- test7.js
+++ test7.js
@@ -3,17 +3,13 @@ import * as base64arraybuffer from 'base64-arraybuffer';
 import earcut from 'earcut';
 import * as THREE from 'three';
 import * as exportSTL from 'Doodle3D/ThreeJS-export-STL';
+import base64font from './Damion-Regular.js';
 
 // jspm run test7
-
+// -> use three.js shapes for font
 // -> dynamically download fonts: https://www.googleapis.com/webfonts/v1/webfonts?key=YOUR-API-KEY
 // -> live preview? / html version?
-// test CSG
-// https://github.com/chandlerprall/ThreeCSG/blob/master/threeCSG.es6
-// jspm install github:Doodle3D/ThreeJS-export-STL
-// ./node_modules/.bin/jspm run test7.js
-
-import base64font from './Damion-Regular.js';
+// -> test CSG: https://github.com/chandlerprall/ThreeCSG/blob/master/threeCSG.es6
 
 function extrudeGlyph(font, glyph, size, width) {
     let metrics = glyph.getMetrics();
@@ -131,31 +127,31 @@ function extrudeString(font, string, size, width, options) {
     return result;
 }
 
-function saveSTL(triangles, filename) {
-    var buffer = 'solid test\n';
-    for (let tri of triangles) {
-        let ab = { x: tri.b.x-tri.a.x, y: tri.b.y-tri.a.y, z: tri.b.z-tri.a.z };
-        let cb = { x: tri.c.x-tri.b.x, y: tri.c.y-tri.b.y, z: tri.c.z-tri.b.z };
-        let cc = {
-            x: ab.y*cb.z - ab.z*cb.y,
-            y: ab.z*cb.x - ab.x*cb.z,
-            z: ab.x*cb.y - ab.y*cb.x,
-        }
-        let ccl = Math.sqrt(cc.x*cc.x + cc.y*cc.y + cc.z*cc.z);
-        cc.x /= ccl;
-        cc.y /= ccl;
-        cc.z /= ccl;
-        buffer += 'facet normal ' + cc.x + ' ' + cc.y + ' ' + cc.z + '\n';
-        buffer += 'outer loop\n';
-        buffer += 'vertex ' + tri.a.x + ' ' + tri.a.y + ' ' + tri.a.z + '\n';
-        buffer += 'vertex ' + tri.b.x + ' ' + tri.b.y + ' ' + tri.b.z + '\n';
-        buffer += 'vertex ' + tri.c.x + ' ' + tri.c.y + ' ' + tri.c.z + '\n';
-        buffer += 'endloop\n';
-        buffer += 'endfacet\n';
-    }
-    buffer += 'endsolid test\n';
-    require('fs').writeFileSync(filename, buffer);    
-}
+// function saveSTL(triangles, filename) {
+//     var buffer = 'solid test\n';
+//     for (let tri of triangles) {
+//         let ab = { x: tri.b.x-tri.a.x, y: tri.b.y-tri.a.y, z: tri.b.z-tri.a.z };
+//         let cb = { x: tri.c.x-tri.b.x, y: tri.c.y-tri.b.y, z: tri.c.z-tri.b.z };
+//         let cc = {
+//             x: ab.y*cb.z - ab.z*cb.y,
+//             y: ab.z*cb.x - ab.x*cb.z,
+//             z: ab.x*cb.y - ab.y*cb.x,
+//         }
+//         let ccl = Math.sqrt(cc.x*cc.x + cc.y*cc.y + cc.z*cc.z);
+//         cc.x /= ccl;
+//         cc.y /= ccl;
+//         cc.z /= ccl;
+//         buffer += 'facet normal ' + cc.x + ' ' + cc.y + ' ' + cc.z + '\n';
+//         buffer += 'outer loop\n';
+//         buffer += 'vertex ' + tri.a.x + ' ' + tri.a.y + ' ' + tri.a.z + '\n';
+//         buffer += 'vertex ' + tri.b.x + ' ' + tri.b.y + ' ' + tri.b.z + '\n';
+//         buffer += 'vertex ' + tri.c.x + ' ' + tri.c.y + ' ' + tri.c.z + '\n';
+//         buffer += 'endloop\n';
+//         buffer += 'endfacet\n';
+//     }
+//     buffer += 'endsolid test\n';
+//     require('fs').writeFileSync(filename, buffer);    
+// }
 
 function toTHREE(triangles) {
     let geometry = new THREE.Geometry();
@@ -173,45 +169,64 @@ function toTHREE(triangles) {
 }
 
 function test(font, string, size, width) {
-    font.forEachGlyph(string, 0, 0, size, options, (glyph, x, y) => {
-        let metrics = glyph.getMetrics();
-        let fontScale = 1 / font.unitsPerEm * size;
+    let path = new THREE.Shape(); // Shape? ShapePath?
+    path.moveTo(0, 0);
+    path.lineTo(100, 0);
+    path.lineTo(100, 100);
+    path.lineTo(0, 100);
 
-        let segments = [];
-        let holes = [];
-        for (let contour of glyph.getContours()) {
-            var sum = 0;
-            var lastPoint = contour[contour.length-1];
-            for (let point of contour) {
-                sum += (lastPoint.x - point.x) * (point.y + lastPoint.y);
-                lastPoint = point;
-            }
-            if (sum > 0) {
-                holes.push(contour);
-            } else {
-                segments.push(contour);
-            }
-        }
+    console.log('XX', THREE.QuadraticBezier);
 
-
+    let geometry = new THREE.ExtrudeGeometry(path, {
+        steps: 2,
+        amount: 16,
+        bevelEnabled: true,
+        bevelThickness: 1,
+        bevelSize: 1,
+        bevelSegments: 1
     });
-}
 
-// THREE to binary STL...
+    return geometry;
+
+    // path.quadraticCurveTo( cpx1, cpy1, cpx, cpy );
+
+    // for ( var i2 = 1; i2 <= divisions; i2 ++ ) {
+    // var t = i2 / divisions;
+    // QuadraticBezier( t, cpx0, cpx1, cpx );
+    // QuadraticBezier( t, cpy0, cpy1, cpy );
+    // }
+
+
+    // font.forEachGlyph(string, 0, 0, size, options, (glyph, x, y) => {
+    //     let metrics = glyph.getMetrics();
+    //     let fontScale = 1 / font.unitsPerEm * size;
+    //     let segments = [];
+    //     let holes = [];
+    //     for (let contour of glyph.getContours()) {
+    //         var sum = 0;
+    //         var lastPoint = contour[contour.length-1];
+    //         for (let point of contour) {
+    //             sum += (lastPoint.x - point.x) * (point.y + lastPoint.y);
+    //             lastPoint = point;
+    //         }
+    //         if (sum > 0) {
+    //             holes.push(contour);
+    //         } else {
+    //             segments.push(contour);
+    //         }
+    //     }
+    // });
+}
 
 async function main() {
     let font = opentype.parse(base64arraybuffer.decode(base64font));
     // let triangles = extrudeGlyph(font, font.charToGlyph('8'), 72, 20);
-    let triangles = extrudeString(font, 'Hallo', 72, 20, {dx: 0});
-    let geometry = toTHREE(triangles);
-
+    // let triangles = extrudeString(font, 'Hallo', 72, 20, {dx: 0});
+    // let geometry = toTHREE(triangles);
+    let geometry = test(font, 'Hallo', 72, 20);
+    geometry.type = 'Geometry'; // bug in exportSTL?
     let data = exportSTL.fromGeometry(geometry);
-    console.log('data', data);
-
     System._nodeRequire('fs').writeFileSync('test.stl', Buffer.from(base64arraybuffer.encode(data), 'base64'));
-
-
-    // saveSTL(triangles, 'test.stl');
 }
 
 main().catch(console.error);
